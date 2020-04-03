2134057dd13ce16261ff53a2af1ca3f227c94590
James Stevenson <info@presencewebdesign.co.uk>
2020-04-03 16:53:25 +0100

    create a profile with user Auth

--- src/App.js
+++ src/App.js
@@ -5,42 +5,51 @@ import HomePage from './pages/homepage/';
 import ShopPage from './pages/shop/';
 import Header from './components/header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up';
-import { auth } from './firebase/';
+import { auth, createUserProfileDocument } from './firebase/';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
+   constructor() {
+      super();
+      this.state = {
+         currentUser: null,
+      };
+   }
 
-  unsubscribeFromAuth = null;
+   unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user,
+   componentDidMount() {
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+         if (userAuth) {
+            const userRef = await createUserProfileDocument(userAuth);
+            userRef.onSnapshot(snapShot => {
+               this.setState({
+                  currentUser: {
+                     id: snapShot.id,
+                     ...snapShot.data(),
+                  },
+               });
+            });
+         }
+         this.setState({ currentUser: userAuth });
       });
-      console.log('user', user);
-    });
-  }
+   }
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+   componentWillUnmount() {
+      this.unsubscribeFromAuth();
+   }
 
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />{' '}
-        <Switch>
-          <Route exact path="/" component={HomePage} /> <Route path="/shop" component={ShopPage} />{' '}
-          <Route path="/signin" component={SignInAndSignUp} />{' '}
-        </Switch>{' '}
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <Header currentUser={this.state.currentUser} />{' '}
+            <Switch>
+               <Route exact path="/" component={HomePage} />{' '}
+               <Route path="/shop" component={ShopPage} />{' '}
+               <Route path="/signin" component={SignInAndSignUp} />{' '}
+            </Switch>{' '}
+         </div>
+      );
+   }
 }
 
 export default App;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,41 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCtgaWzMTtqnbbKQddtljQaHLkbko-kWnY',
-  authDomain: 'ecommerce-65f8b.firebaseapp.com',
-  databaseURL: 'https://ecommerce-65f8b.firebaseio.com',
-  projectId: 'ecommerce-65f8b',
-  storageBucket: 'ecommerce-65f8b.appspot.com',
-  messagingSenderId: '879961457400',
-  appId: '1:879961457400:web:6feae243de277ca548310d',
-  measurementId: 'G-FXVZF1DGPD',
+   apiKey: 'AIzaSyCtgaWzMTtqnbbKQddtljQaHLkbko-kWnY',
+   authDomain: 'ecommerce-65f8b.firebaseapp.com',
+   databaseURL: 'https://ecommerce-65f8b.firebaseio.com',
+   projectId: 'ecommerce-65f8b',
+   storageBucket: 'ecommerce-65f8b.appspot.com',
+   messagingSenderId: '879961457400',
+   appId: '1:879961457400:web:6feae243de277ca548310d',
+   measurementId: 'G-FXVZF1DGPD',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+
+   const userRef = firestore.doc(`users/${userAuth.uid}`);
+   const snapShot = await userRef.get();
+
+   console.log('snapShot', snapShot);
+
+   if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+         await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData,
+         });
+      } catch (error) {
+         console.log('eror creating user');
+      }
+   }
+
+   return userRef;
 };
 
 firebase.initializeApp(config);
@@ -19,7 +46,7 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: 'select_account',
+   prompt: 'select_account',
 });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
