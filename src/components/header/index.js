e5a1e7eb7281b7ceafbbd83b61adae29d5fdb46f
James Stevenson <info@presencewebdesign.co.uk>
2020-04-04 19:18:31 +0100

    set up redux reducers, store

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,28 +5,28 @@ import './style.scss';
 import { auth } from '../../firebase/';
 
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
-      )}
-    </div>
-  </div>
+   <div className="header">
+      <Link className="logo-container" to="/">
+         <Logo className="logo" />
+      </Link>{' '}
+      <div className="options">
+         <Link className="option" to="/shop">
+            SHOP{' '}
+         </Link>{' '}
+         <Link className="option" to="/shop">
+            CONTACT{' '}
+         </Link>{' '}
+         {currentUser ? (
+            <div className="option" onClick={() => auth.signOut()}>
+               SIGN OUT{' '}
+            </div>
+         ) : (
+            <Link className="option" to="/signin">
+               SIGN IN{' '}
+            </Link>
+         )}
+      </div>{' '}
+   </div>
 );
 
 export default Header;
