03808ae572cef260f78e36b6909d7cf4f810432d
James Stevenson <info@presencewebdesign.co.uk>
2020-04-13 13:36:00 +0100

    build checkout and add total count

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './style.scss';
 
-const CheckoutPage = () => <div>Checkout Page </div>;
+const CheckoutPage = ({ cartItems, total }) => (
+   <div className="checkout-page">
+      <div className="checkout-header">
+         <div className="header-blocks">
+            <span> Product </span>{' '}
+         </div>{' '}
+         <div className="header-blocks">
+            <span> Description </span>{' '}
+         </div>{' '}
+         <div className="header-blocks">
+            <span> Quantity </span>{' '}
+         </div>{' '}
+         <div className="header-blocks">
+            <span> Price </span>{' '}
+         </div>{' '}
+         <div className="header-blocks">
+            <span> remove </span>{' '}
+         </div>{' '}
+      </div>
+      {cartItems.map((cartItem) => cartItems.name)}
 
-export default CheckoutPage;
+      <div className="total">
+         <span>TOTAL: ${total}</span>
+      </div>
+   </div>
+);
+
+const mapsStateToProps = createStructuredSelector({
+   cartItems: selectCartItems,
+   total: selectCartTotal,
+});
+
+export default connect(mapsStateToProps)(CheckoutPage);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,3 +9,7 @@ export const selectCartItems = createSelector([selectCart], (cart) => cart.cartI
 export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) =>
    cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0),
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+   cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price, 0),
+);
