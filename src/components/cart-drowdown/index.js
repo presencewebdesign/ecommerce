d7e2c95261e7266a4d2b9950a4a41835b8755072
James Stevenson <info@presencewebdesign.co.uk>
2020-04-13 11:23:51 +0100

    add lib selectors and move functions as an export selector

--- src/components/cart-drowdown/index.js
+++ src/components/cart-drowdown/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../../components/customButton';
 import CartItem from '../cartItem';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import './style.scss';
 
 const CartDropdown = ({ cartItems }) => (
    <div className="cart-dropdown">
       <div className="cart-items">
+         {' '}
          {cartItems.map((cartItem) => (
             <CartItem key={cartItem.id} item={cartItem} />
-         ))}
+         ))}{' '}
       </div>{' '}
       <CustomButton> GO TO CHECKOUT </CustomButton>{' '}
    </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-   cartItems,
+const mapStateToProps = (state) => ({
+   cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/index.js
+++ src/components/cart-icon/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { ReactComponent as ShopppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import './style.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
    <div className="cart-icon" onClick={toggleCartHidden}>
-      <ShopppingIcon className="shopping-icon">
-         <span className="icon-count"> 0 </span>{' '}
-      </ShopppingIcon>{' '}
+      <ShopppingIcon className="shopping-icon" />
+      <span className="item-count"> {itemCount} </span>{' '}
    </div>
 );
 
@@ -17,4 +16,8 @@ const mapDispatchToProps = (dispatch) => ({
    toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+   itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
