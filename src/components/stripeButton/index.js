4a91efb4e71444c1ccaf88be6d74da39d3998285
James Stevenson <info@presencewebdesign.co.uk>
2020-04-21 00:01:36 +0100

    add integration for Stripe

---
+++ src/components/stripeButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const stripeCheckoutButton = ({ price }) => {
+   const priceForStripe = price * 100;
+   const publishableKey = 'pk_test_lgM8XRZqQyeVIDTUKmVSoF6700Qf4rp6ya';
+   const onToken = (token) => {
+      console.log(token);
+      alert('Payment Successfull');
+   };
+   return (
+      <StripeCheckout
+         label="Pay Now"
+         name="Presence Clothing"
+         billingAddress
+         shippingAddress
+         image="https://presence.agency/wp-content/themes/presence/img/presence-logo.png"
+         descriptiom={`Tour total is £${price}`}
+         amount={priceForStripe}
+         panelLabel="Pay now"
+         token={onToken}
+         stripeKey={publishableKey}
+      />
+   );
+};
+
+export default stripeCheckoutButton;
--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkoutItem';
+import StripeCheckoutButton from '../../components/stripeButton';
 import './style.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -23,13 +24,18 @@ const CheckoutPage = ({ cartItems, total }) => (
          <div className="header-blocks">
             <span> remove </span>{' '}
          </div>{' '}
-      </div>
+      </div>{' '}
       {cartItems.map((cartItem) => (
          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
+      ))}{' '}
       <div className="total">
-         <span>TOTAL: ${total}</span>
+         <span> TOTAL: $ {total} </span>{' '}
+      </div>{' '}
+      <div className="test-warning">
+         *Please use the following test credit card for payments* <br />
+         4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
       </div>
+      <StripeCheckoutButton price={total} />{' '}
    </div>
 );
 
